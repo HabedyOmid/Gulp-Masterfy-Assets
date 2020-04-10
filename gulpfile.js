238f69635b540de25df28fad5f4e52aab0bfb0c7
fluffy <fluffy@fluffys-MacBook-Pro.local>
2020-04-10 12:10:03 -0500

    bootstrap override fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,121 @@
 // node.js Packages / Dependencies
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
-const uglify        = require('gulp-uglify');
-const rename        = require('gulp-rename');
-const concat        = require('gulp-concat');
-const cleanCSS      = require('gulp-clean-css');
-const imageMin      = require('gulp-imagemin');
-const pngQuint      = require('imagemin-pngquant'); 
-const browserSync   = require('browser-sync').create();
-const autoprefixer  = require('gulp-autoprefixer');
-const jpgRecompress = require('imagemin-jpeg-recompress'); 
-const clean         = require('gulp-clean');
-
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
 
 // Paths
 var paths = {
-    root: { 
-        www:        './public_html'
-    },
-    src: {
-        root:       'public_html/assets',
-        html:       'public_html/**/*.html',
-        css:        'public_html/assets/css/*.css',
-        js:         'public_html/assets/js/*.js',
-        vendors:    'public_html/assets/vendors/**/*.*',
-        imgs:       'public_html/assets/img/**/*.+(png|jpg|gif|svg)',
-        scss:       'public_html/assets/scss/**/*.scss'
-    },
-    dist: {
-        root:       'public_html/dist',
-        css:        'public_html/dist/css',
-        js:         'public_html/dist/js',
-        imgs:       'public_html/dist/img',
-        vendors:    'public_html/dist/vendors'
-    }
-}
-
+	root: {
+		www: 'public',
+	},
+	src: {
+		root: 'public/assets',
+		html: 'public/**/*.html',
+		css: 'public/assets/css/*.css',
+		js: 'public/assets/js/*.js',
+		vendors: 'public_html/assets/vendors/**/*.*',
+		imgs: 'public/assets/img/**/*.+(png|jpg|gif|svg)',
+		scss: 'public/assets/scss/**/*.scss',
+	},
+	dist: {
+		root: 'public/dist',
+		css: 'public/dist/css',
+		js: 'public/dist/js',
+		imgs: 'public/dist/img',
+		vendors: 'public/dist/vendors',
+	},
+};
 
 // Compile SCSS
-gulp.task('sass', function() {
-    return gulp.src(paths.src.scss)
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) 
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(paths.src.root + '/css'))
-    .pipe(browserSync.stream());
+gulp.task('sass', () => {
+	return gulp
+		.src(paths.src.scss)
+		.pipe(
+			sass({
+				outputStyle: 'expanded',
+			}).on('error', sass.logError),
+		)
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(paths.src.root + '/css'))
+		.pipe(browserSync.stream());
 });
-
 
 // Minify + Combine CSS
-gulp.task('css', function() {
-    return gulp.src(paths.src.css)
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(concat('app.css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.dist.css))
+gulp.task('css', () => {
+	return gulp
+		.src(paths.src.css)
+		.pipe(
+			cleanCSS({
+				compatibility: 'ie8',
+			}),
+		)
+		.pipe(concat('app.css'))
+		.pipe(
+			rename({
+				suffix: '.min',
+			}),
+		)
+		.pipe(gulp.dest(paths.dist.css));
 });
-
 
 // Minify + Combine JS
-gulp.task('js', function() {
-    return gulp.src(paths.src.js)
-    .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest(paths.dist.js))
-    .pipe(browserSync.stream());
+gulp.task('js', () => {
+	return gulp
+		.src(paths.src.js)
+		.pipe(uglify())
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest(paths.dist.js))
+		.pipe(browserSync.stream());
 });
-
 
 // Compress (JPEG, PNG, GIF, SVG, JPG)
-gulp.task('img', function(){
-    return gulp.src(paths.src.imgs)
-    .pipe(imageMin([
-        imageMin.gifsicle(),
-        imageMin.jpegtran(),
-        imageMin.optipng(),
-        imageMin.svgo(),
-        pngQuint(),
-        jpgRecompress()
-    ]))
-    .pipe(gulp.dest(paths.dist.imgs));
+gulp.task('img', () => {
+	return gulp
+		.src(paths.src.imgs)
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.mozjpeg({ quality: 75, progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }),
+				imagemin.svgo({
+					plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+				}),
+			]),
+		)
+		.pipe(gulp.dest(paths.dist.imgs));
 });
-
 
 // copy vendors to dist
-gulp.task('vendors', function(){
-    return gulp.src(paths.src.vendors)
-    .pipe(gulp.dest(paths.dist.vendors))
+gulp.task('vendors', () => {
+	return gulp.src(paths.src.vendors).pipe(gulp.dest(paths.dist.vendors));
 });
-
 
 // clean dist
 gulp.task('clean', function () {
-    return gulp.src(paths.dist.root)
-        .pipe(clean());
+	return gulp.src(paths.dist.root).pipe(clean());
 });
-
 
 // Prepare all assets for production
 gulp.task('build', gulp.series('sass', 'css', 'js', 'img'));
 
-
 // Watch (SASS, CSS, JS, and HTML) reload browser on change
-gulp.task('watch', function() {
-    browserSync.init({
-        server: {
-            baseDir: paths.root.www
-        } 
-    })
+gulp.task('watch', () => {
+	browserSync.init({
+		server: {
+			baseDir: paths.root.www,
+		},
+	});
 
-    gulp.watch(paths.src.scss, gulp.series('sass'));
-    gulp.watch(paths.src.css, gulp.series('css'));
-    gulp.watch(paths.src.js, gulp.series('js'));
-    gulp.watch(paths.src.html).on('change', browserSync.reload);
+	gulp.watch(paths.src.scss, gulp.series('sass'));
+	gulp.watch(paths.src.css, gulp.series('sass'));
+	gulp.watch(paths.src.js, gulp.series('js'));
+	gulp.watch(paths.src.html).on('change', browserSync.reload);
 });
